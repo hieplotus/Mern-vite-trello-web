9a4be0eff10b34c8ff0a8a4cc6a1bb1b0f0a1425
unknown <hieph@lotusoutsourcing.com>
2023-07-14 14:06:50 +0700

    Refactor theme color

--- src/pages/Boards/BoardContent/index.jsx
+++ src/pages/Boards/BoardContent/index.jsx
@@ -1,17 +1,17 @@
 import Box from '@mui/material/Box'
 
-function index() {
+function BoardContent() {
   return (
     <Box sx={{
-      backgroundColor: 'primary.main',
+      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e': '#1976d2'),
       width: '100%',
       height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})` ,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center'   
     }}>
 			Board Content
     </Box>
   )
 }
 
-export default index
+export default BoardContent
--- src/theme.js
+++ src/theme.js
@@ -10,18 +10,18 @@ const theme = extendTheme({
     boardBarHeight: '60px'
   },
   colorSchemes: {
-    light: {
-      palette: {
-        primary: teal,
-        secondary: deepOrange
-      }
-    },
-    dark: {
-      palette: {
-        primary: cyan,
-        secondary: orange
-      }
-    }
+    // light: {
+    //   palette: {
+    //     primary: teal,
+    //     secondary: deepOrange
+    //   }
+    // },
+    // dark: {
+    //   palette: {
+    //     primary: cyan,
+    //     secondary: orange
+    //   }
+    // }
   },
   components: {
     MuiCssBaseline: {
@@ -32,11 +32,11 @@ const theme = extendTheme({
             height: '8px'
           },
           '*::-webkit-scrollbar-thumb': {
-            backgroundColor: '#bdc3c7',
+            backgroundColor: '#dcdde1',
             borderRadius: '8px'
           },
           '*::-webkit-scrollbar-thumb:hover': {
-            backgroundColor: '#00b894'
+            backgroundColor: 'white'
           }
         }
       }
@@ -44,7 +44,9 @@ const theme = extendTheme({
     MuiButton: {
       styleOverrides: {
         root: {
-          textTransform: 'none'
+          textTransform: 'none',
+          borderWidth: '0.5px',
+          '&:hover': { borderWidth: '2px'}
         }
       }
     },
@@ -61,19 +63,19 @@ const theme = extendTheme({
         root: ({ theme }) => {
           // console.log('theme', theme)
           return {
-            color: theme.palette.primary.main,
-            fontSize: '0.875rem',
-            '.MuiOutlinedInput-notchedOutline': {
-              borderColor: theme.palette.primary.light
-            },
-            '&:hover': {
-              '.MuiOutlinedInput-notchedOutline': {
-                borderColor: theme.palette.primary.main
-              }
-            },
-            '& fieldset': {
-              borderWidth: '1px !important'
-            }
+            // color: theme.palette.primary.main,
+            // fontSize: '0.875rem',
+            // '.MuiOutlinedInput-notchedOutline': {
+            //   borderColor: theme.palette.primary.light
+            // },
+            // '&:hover': {
+            //   '.MuiOutlinedInput-notchedOutline': {
+            //     borderColor: theme.palette.primary.main
+            //   }
+            // },
+            '& fieldset': {borderWidth: '0.25px !important' },
+            '&:hover fieldset': {borderWidth: '2px !important' },
+            '&.Mui-focused fieldset': {borderWidth: '2px !important' }
           }
         }
       }
